8f1dbe7f2536f11438b15a353dcd97dcb2e1c8c3
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-06-10 20:41:20 -0300

    parameters for LAMMPS

--- aelm.py
+++ aelm.py
@@ -25,7 +25,15 @@ import numpy as np
 import pandas as pd
 
 
-def aelm(biased_traj, minimizations_output, cell_info):
+def aelm(
+    biased_traj,
+    minimizations_output,
+    cell_info,
+    lmp_exec="./lmp",
+    lmp_in="in.minimization",
+    lmp_min="dump.minimization.lammpstrj",
+    lmp_flags=None,
+):
     """Accelerated exploration of local minima minimization.
 
     Parameters
@@ -42,41 +50,67 @@ def aelm(biased_traj, minimizations_output, cell_info):
         correspondence between the elements present in xyz file with integer
         identification numbers, e.g. {"Si": 1, "Li": 2}
 
+    lmp_exec : str, default="./lmp"
+        the path and name of the LAMMPS executable
+
+    lmp_in : str, default="in.minimization"
+        the input file for LAMMPS
+
+    lmp_min : str, default="dump.minimization.lammpstrj"
+        the dump file where the local minimization is written
+
+    lmp_flags : dict, default=None
+        command-line options for LAMMPS, where the key is the flag and the value
+        the option.
+
     Returns
     -------
     pd.DataFrame
         A `pd.DataFrame with three columsn: the initial, the next to last and
-        final energies of each frame.
+        final energies of each frame. It also generates a lammpstrj file with
+        all the minimum energy frames in the same order as the initial biased
+        xyz file.
     """
     initial, next_to_last, final = [], [], []
     with exma.io.reader.XYZ(biased_traj, ftype="xyz") as bias:
         try:
             while True:
+                # read and xyz frame
                 frame = bias.read_frame()
                 frame.box = cell_info["box"]
                 frame.idx = np.arange(1, frame.natoms + 1)
                 frame.types = [cell_info["type"][t] for t in frame.types]
                 frame.q = np.zeros(frame.natoms, dtype=np.float32)
 
+                # write the frame to a configurations input file for lammps
                 exma.io.writer.in_lammps("in.frame", frame)
 
-                # run lammps minimization and get the energy data and last
-                # frame of the structure:
-                os.system("./lmp_serial -in in.minimization")
-                os.system(
-                    "awk '/Energy\ initial/,/Force\ two-norm/' log.lammps | sed '1d; $d' > emin.dat"
-                )
-                os.system(
-                    f"tail -{frame.natoms + 9} dump.minimization.lammpstrj >> dump.all.lammpstrj"
-                )
+                # run lammps minimization with the corresponding flags
+                run_cmd = f"{lmp_exec} -in {lmp_in}"
+                if lmp_flags is not None:
+                    for key, value in lmp_flags.items():
+                        if key != "screen" and value != "none":
+                            run_cmd += f" -{key} {value}"
+                os.system(run_cmd)
 
-                e1, e2, e3 = np.loadtxt("emin.dat", unpack=True, dtype=np.float32)
-
+                # get the energies and save for the pd.DataFrame
+                os.system(
+                    f"awk '/Energy\ initial/,/Force\ two-norm/' log.lammps | sed '1d; $d' > emin.dat"
+                )
+                e1, e2, e3 = np.loadtxt(
+                    "emin.dat", unpack=True, dtype=np.float32
+                )
                 initial.append(e1)
                 next_to_last.append(e2)
                 final.append(e3)
 
-                os.system("rm in.frame dump.minimization.lammpstrj log.* emin.dat")
+                # accumulate the minimum energy frames
+                os.system(
+                    f"tail -{frame.natoms + 9} {lmp_min} >> dump.all.lammpstrj"
+                )
+
+                # remove tmp files
+                os.system("rm in.frame {lmp_min} log.* emin.dat")
 
         except EOFError:
             ...
